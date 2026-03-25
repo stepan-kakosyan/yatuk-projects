8c7ee6b4ac4f83c107662982427e6bc057978364
Stepan Kakosyan <stepankakosyan22@gmail.com>
2026-03-25 13:18:44 +0400

    CI CD workflow

--- yatukplay/utils/slugs.py
+++ yatukplay/utils/slugs.py
@@ -14,6 +14,7 @@ def unique_slug_generator(instance, new_slug=None):
         slug = slugify(instance.name_en.lower(), allow_unicode=True)
     cl = instance.__class__
     qs_exists = cl.objects.filter(slug=slug).exists()
+
     if qs_exists:
         new_slug = f"{slug}-{random_string_generator(size = 2)}"
         return unique_slug_generator(instance, new_slug=new_slug)
